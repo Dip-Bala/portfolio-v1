6a558afb61e8e07c6a0797ee89db3629c30136f1
Dipanwita Bala <106340756+Dip-Bala@users.noreply.github.com>
2025-07-24 23:59:24 +0530

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 function App() {
   return (
-    <div className="bg-[#1a1a1a] w-screen h-screen flex flex-col justify-center items-center text-[#edece7]">
-      <h2 className="font-sans font-bold text-9xl font-stretch-extra-condensed tracking-wider">PORTFOLIO</h2>
-      <p className="italic font-serif">Ms Bala</p>
+    <div>
+      
     </div>
   )
 }
